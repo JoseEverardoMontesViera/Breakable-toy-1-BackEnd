82b5afa1b6a9522394b354641a51d61368dc3054
Everardo <everardo.montes@encora.com>
2024-12-12 11:49:24 -0600

    first commit

---
+++ src/main/java/com/example/demo/BreakableToy1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakableToy1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakableToy1BackendApplication.class, args);
+	}
+
+}
